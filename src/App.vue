ca155d7b1f0608c97dbcb796fa4699a080c45bb1
zhouyang8888 <zhouyang@DESKTOP-GIPP3S9.localdomain>
2021-01-21 14:37:35 +0800

    MOD:trivial

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <!--router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link-->
-      <router-link to="/frontpage">Frontpage</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/">Frontpage</router-link-->
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Frontpage from '../views/Frontpage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Frontpage',
+    component: Frontpage
+  },
+  /*
+  {
+    path: '/',
     name: 'Home',
     component: Home
   },
+  */
   {
     path: '/about',
     name: 'About',
@@ -17,15 +24,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/frontpage',
-    name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Frontpage.vue')
-  }
+  }// ,
+  // {
+  // path: '/frontpage',
+  // name: 'Frontpage',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ '../views/Frontpage.vue')
+  // }
 ]
 
 const router = new VueRouter({
