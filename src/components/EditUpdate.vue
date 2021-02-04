39219d8fcb110a0b1a6cc52901140d7e87d7ea38
zhouyang8888 <zhouyang@DESKTOP-GIPP3S9.localdomain>
2021-02-04 16:43:03 +0800

    MOD:v-model bind input text with model values.

--- src/components/EditUpdate.vue
+++ src/components/EditUpdate.vue
@@ -1,19 +1,20 @@
 <template>
     <div name='update' class='uproblems'>
       <div>
-        <p id='pid'>题号</p><input type="text" ref="queryID" id="queryID" class="text" onmouseover="this.select()" v-on:input="searchByID" placeholder="填入题号" />
+        <p id='pid'>题号</p>
+        <input type='text' id='questID' class='text' onmouseover="this.select()" v-on:input="searchByID" placeholder='填入题号' v-model="questID"/>
         <button type='submit' v-on:click="searchByID" >查询</button>
-        <p id='pstatus' ref='pstatus' style="display:none;" />
+        <p id='pstatus' :style="pstatusStyle" >{{ pstatusText }}</p>
       </div>
-      <div><p>title</p><input type='text' ref='title' id='title' class='text' /></div>
+      <div><p>title</p><input type='text' class='text' v-model="title"/></div>
       <div>
-        <p>desc</p><textarea  ref='desc' id='desc' class='text'  />
+        <p>desc</p><textarea  class='text' v-model="desc" />
         <img src='' ref='newimg' id='newimg' />
         <input type='file' ref='saveImage' id='saveImage' name="saveImage" v-on:change="selectedImage" />
         <input type='submit' ref='upImage' id='upImage' v-on:click="uploadImage" />
       </div>
-      <div><p>in</p><input type='text' ref='in' id='in' class='text'  /></div>
-      <div><p>out</p><input type='text' ref='out' id='out' class='text'  /></div>
+      <div><p>in</p><input type='text' class='text' v-model="input" /></div>
+      <div><p>out</p><input type='text' class='text' v-model="output" /></div>
       <button type='submit' v-on:click="update()" >更新</button>
       <button type='sumbit' v-on:click="del()" >删除</button>
     </div>
@@ -26,23 +27,27 @@ export default {
   name: 'EditUpdate',
   props: {
   },
-  computed: {
+  data: function () {
+    return {
+      questID: '',
+      pstatusStyle: 'display:none;',
+      pstatusText: '',
+      title: '',
+      desc: '',
+      input: '',
+      output: ''
+    }
   },
   methods: {
     getQueryID: function () {
-      const questID = this.$refs.queryID.value.trim()
-      // document.getElementById('queryID').value.trim()
+      const questID = this.questID.trim()
       if (questID === '') {
-        this.$refs.pstatus.style = 'display:inline;color:red;font-size:smaller'
-        this.$refs.pstatus.textContent = '<==填入题号'
-        // document.getElementById('pstatus').style = 'display:inline;color:red;font-size:smaller'
-        // document.getElementById('pstatus').textContent = '<==输入题号'
+        this.pstatusStyle = 'display:inline;color:red;font-size:smaller'
+        this.pstatusText = '<==填入题号'
         return { status: false }
       } else if (!questID.match(/^[0-9]+$/)) {
-        this.$refs.pstatus.style = 'display:inline;color:red;font-size:smaller'
-        this.$refs.pstatus.textContent = '题号只能包含0-9的数字'
-        // document.getElementById('pstatus').style = 'display:inline;color:red;font-size:smaller'
-        // document.getElementById('pstatus').textContent = '题号只能包含0-9的数字'
+        this.pstatusStyle = 'display:inline;color:red;font-size:smaller'
+        this.pstatusText = '题号只能包含0-9的数字'
         return { status: false }
       } else {
         return { status: true, questID: questID }
@@ -53,24 +58,16 @@ export default {
       if (ret.status === false) return
       const questID = ret.questID
 
-      // const title = document.getElementById('title').value.trim()
-      // const desc = document.getElementById('desc').value.trim()
-      // const input = document.getElementById('in').value.trim()
-      // const output = document.getElementById('out').value.trim()
-      // const postBody = { pid: questID, t: title, d: desc, i: input, o: output }
-
-      const title = this.$refs.title.value.trim()
-      const desc = this.$refs.desc.value.trim()
-      const input = this.$refs.in.value.trim()
-      const output = this.$refs.out.value.trim()
+      const title = this.title.trim()
+      const desc = this.desc.trim()
+      const input = this.input.trim()
+      const output = this.output.trim()
       const postBody = { pid: questID, t: title, d: desc, i: input, o: output }
       await axios.post('http://127.0.0.1:80/api/updateByID', postBody)
         .then(response => {
           if (title === response.data.title && desc === response.data.desc && input === response.data.in && output === response.data.out && response.data.valid) {
-            this.$refs.pstatus.style = 'display:inline;color:red;font-size:smaller'
-            this.$refs.pstatus.textContent = '更新成功'
-            // document.getElementById('pstatus').style = 'display:inline;color:red;font-size:smaller'
-            // document.getElementById('pstatus').textContent = '更新成功'
+            this.pstatusStyle = 'display:inline;color:red;font-size:smaller'
+            this.pstatusText = '更新成功'
           }
         })
         .catch(err => { alert(err) })
@@ -83,19 +80,13 @@ export default {
       const postBody = { pid: questID }
       await axios.post('http://127.0.0.1:80/api/deleteByID', postBody)
         .then(response => {
-          // const title = document.getElementById('title').value.trim()
-          // const desc = document.getElementById('desc').value.trim()
-          // const input = document.getElementById('in').value.trim()
-          // const output = document.getElementById('out').value.trim()
-          const title = this.$refs.title.value.trim()
-          const desc = this.$refs.desc.value.trim()
-          const input = this.$refs.in.value.trim()
-          const output = this.$refs.out.value.trim()
+          const title = this.title.trim()
+          const desc = this.desc.trim()
+          const input = this.input.trim()
+          const output = this.output.trim()
           if (title === response.data.title && desc === response.data.desc && input === response.data.in && output === response.data.out && !response.data.valid) {
-            this.$refs.pstatus.style = 'display:inline;color:red;font-size:smaller'
-            this.$refs.pstatus.textContent = '删除成功'
-            // document.getElementById('pstatus').style = 'display:inline;color:red;font-size:smaller'
-            // document.getElementById('pstatus').textContent = '删除成功'
+            this.pstatusStyle = 'display:inline;color:red;font-size:smaller'
+            this.pstatusText = '删除成功'
           }
         })
         .catch(err => { alert(err) })
@@ -109,35 +100,22 @@ export default {
       await axios.post('http://127.0.0.1:80/api/searchByID', postBody)
         .then(response => {
           if (!response.data) {
-            // document.getElementById('title').value = ''
-            // document.getElementById('desc').value = ''
-            // document.getElementById('in').value = ''
-            // document.getElementById('out').value = ''
-            // document.getElementById('pstatus').style = 'display:inline;color:red;font-size:smaller'
-            // document.getElementById('pstatus').textContent = '不存在该ID的题目!!!'
-            this.$refs.title.value = ''
-            this.$refs.desc.value = ''
-            this.$refs.in.value = ''
-            this.$refs.out.value = ''
-            this.$refs.pstatus.style = 'display:inline;color:red;font-size:smaller'
-            this.$refs.pstatus.textContent = '不存在该ID的题目!!!'
+            this.title = ''
+            this.desc = ''
+            this.input = ''
+            this.output = ''
+            this.pstatusStyle = 'display:inline;color:red;font-size:smaller'
+            this.pstatusText = '不存在该ID的题目!!!'
           } else {
-            // document.getElementById('title').value = response.data.title
-            // document.getElementById('desc').value = response.data.desc
-            // document.getElementById('in').value = response.data.in
-            // document.getElementById('out').value = response.data.out
-            this.$refs.title.value = response.data.title
-            this.$refs.desc.value = response.data.desc
-            this.$refs.in.value = response.data.in
-            this.$refs.out.value = response.data.out
+            this.title = response.data.title
+            this.desc = response.data.desc
+            this.input = response.data.in
+            this.output = response.data.out
             if (response.data.valid) {
-              this.$refs.pstatus.style = 'display:none;'
-              // document.getElementById('pstatus').style = 'display:none;'
+              this.pstatusStyle = 'display:none;'
             } else {
-              this.$refs.pstatus.style = 'display:inline;color:red;font-size:smaller'
-              this.$refs.pstatus.textContent = '该题目已经删除，可点击“更新”重启'
-              // document.getElementById('pstatus').style = 'display:inline;color:red;font-size:smaller'
-              // document.getElementById('pstatus').textContent = '该题目已经删除，可点击“更新”重启'
+              this.pstatusStyle = 'display:inline;color:red;font-size:smaller'
+              this.pstatusText = '该题目已经删除，可点击“更新”重启'
             }
           }
         })
@@ -167,7 +145,7 @@ export default {
       }
       await axios.post('http://127.0.0.1:80/api/saveImg', params, config)
         .then(response => {
-          console.log(JSON.stringify(response.data))
+          alert(JSON.stringify(response.data))
         })
         .catch(function (error) {
           console.log(error)
@@ -213,7 +191,7 @@ export default {
   border-radius: 5px;
   border-width: 1px;
 }
-#queryID {
+#questID {
   width: 20%;
   border-radius: 5px;
   border-width: 1px;
